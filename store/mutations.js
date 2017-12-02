34c79b7762ed836b865cf1d1fa44d5a58454dd6c
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2017-12-02 15:10:02 +0100

    Delete project if no deployments

--- store/mutations.js
+++ store/mutations.js
@@ -2,6 +2,7 @@ import _assign from 'lodash/assign'
 import _isNil from 'lodash/isNil'
 import _isUndefined from 'lodash/isUndefined'
 import _each from 'lodash/each'
+import _filter from 'lodash/filter'
 import _find from 'lodash/find'
 import _findIndex from 'lodash/findIndex'
 import Vue from 'vue'
@@ -21,8 +22,16 @@ export default {
     Vue.delete(state.auth.tokens, deleteIndex)
   },
   [types.DELETE_DEPLOYMENT] (state, { deployment }) {
+    const projectName = deployment.name
+    const isEmptyProject = _filter(state.deployments, { name: projectName }).length <= 0
     const deleteIndex = _findIndex(state.deployments, { uid: deployment.uid })
+
     Vue.delete(state.deployments, deleteIndex)
+
+    if (isEmptyProject) {
+      const deleteProjectIndex = _findIndex(state.deploymentProjects, { name: projectName })
+      Vue.delete(state.deploymentProjects, deleteProjectIndex)
+    }
   },
   [types.DELETE_DEPLOYMENT_ALIAS] (state, { deployment, alias }) {
     const existingDeployment = _find(state.deployments, { uid: deployment.uid })
